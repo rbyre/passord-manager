6646f44eb46e23c0ff2f157466a635daf118dda1
Runar Byre <runar.byre@gmail.com>
2023-01-07 08:26:36 +0100

    lagrer json add search and update

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 import random as r
 import pyperclip
+import json
 
 
 
@@ -23,34 +24,59 @@ def save_password():
   website = website_entry.get()
   username = user_entry.get()
   password = password_entry.get()
-  mydata = f"{website} | {username} | {password}"
+  new_data = {
+    website: {
+      "username": username,
+      "password": password,
+    }
+  }
   if len(website) > 0 and len(password) > 0:
-    is_ok = messagebox.askokcancel(title=website,message=f"Du prøve å lagra følgande: {mydata} e det ok å lagra?")
+    try:
+        with open('data.json', 'r') as f:
+          data = json.load(f)
+          data.update(new_data)
 
-    if is_ok:
-      with open('data.txt', 'a') as f:
-        f.write(mydata)
-        f.write("\n")
-      website_entry.delete(0, END)
-      password_entry.delete(0,END)
+        with open('data.json', 'w') as f:
+          json.dump(data, f, indent=4)
+          
+          website_entry.delete(0, END)
+          password_entry.delete(0,END)
+    except FileNotFoundError:
+      with open('data.json', 'w') as f:
+        json.dump(new_data, f, indent=4)
   else:
     messagebox.showerror(title="Manglende data", message="Et nødvendig felt er tomt. Vennligst legg inn.")
 
-  
+
+# søk etter passord
+def search(website):
+  try:
+    with open('data.json', 'r') as f:
+      data = json.load(f)
+      user_entry.delete(0,END)
+      user_entry.insert(0, data[website]['username'])
+      password_entry.insert(0, data[website]['password'])
+  except:
+    messagebox.showerror(title="Feil", message="Nettsted ikke funnet")
+
+
   
 canvas = Canvas(width=200, height=200)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image((100,100), image=logo_img )
 canvas.grid(row=0, column=1)
 
-website_label = Label(text="Website:")
+website_label = Label(text="Nettsted:", anchor='w')
 website_label.grid(row=1, column=0)
 
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2, sticky="EW")
+website_entry = Entry()
+website_entry.grid(row=1, column=1, sticky="EW")
 website_entry.focus()
 
-user_label = Label(text="Email/Username:")
+search_button = Button(text="Søk", command=lambda: search(website_entry.get()))
+search_button.grid(row=1, column=2, sticky="EW")
+
+user_label = Label(text="Brukernavn:", anchor='w')
 user_label.grid(row=2, column=0)
 
 
@@ -58,17 +84,18 @@ user_entry = Entry(width=35)
 user_entry.grid(row=2, column=1, columnspan=2, sticky="EW")
 user_entry.insert(0, "runar@email.com")
 
-password_label = Label(text="Password:")
+password_label = Label(text="Password:", anchor='w')
 password_label.grid(row=3, column=0)
 
 password_entry = Entry(width=21)
-password_entry.grid(row=3, column=1)
+password_entry.grid(row=3, column=1, sticky="EW")
 
-generate_password_btn = Button(text="Generate Password", command=generate_password)
+generate_password_btn = Button(text="Generer passord", command=generate_password)
 generate_password_btn.grid(row=3, column=2, sticky="EW")
 
-add_btn = Button(text="Add", width=36, command=save_password)
+add_btn = Button(text="Legg til/Oppdater", width=36, command=save_password)
 add_btn.grid(row=4, column=1, columnspan=2, sticky="EW")
+
 
 
 
